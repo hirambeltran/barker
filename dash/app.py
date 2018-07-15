f281369585b49fc0ecfdd62b71ffddaf862ab6e7
hirambeltran <hirambeltran@gmail.com>
2018-07-15 16:07:30 -0700

    Update app.py

--- dash/app.py
+++ dash/app.py
@@ -19,6 +19,33 @@ app = dash.Dash(__name__)
 server = app.server
 server.secret_key = os.environ.get('secret_key', 'secret')
 
-app.title='Lambda: Shifts and Lags'
+app.title='Mean mortality rates across copies: Barker vs. Background'
 
 # get data
+shift_pred = pd.read_csv('data/summary_results_at_age40.csv')
+
+# menus
+#proportion_Barker = shift_pred.prop.unique()
+#label_proportion_Barker = [w.replace('_', ' ') for w in proportion_Barker]
+#dict_proportion_Barker = dict(zip(prop, label_proportion_Barker))
+
+excess_Barker = shift_pred.R.unique()
+label_excess_Barker = [w.replace('_', ' ') for w in excess_Barker]
+dict_excess_Barker = dict(zip(R, label_excess_Barker))
+
+
+
+# colors
+dict_colors_shift = dict(zip(['1.5', '2', '2.5', '3', '3.5', '4'],
+                       ['#e34a33', '#2b8cbe', '#31a354', '#fdae6b', '#e34a33', '#2b8cbe']))
+
+dict_colors_back = dict(zip(['Background'],
+                       ['#31a354']))
+
+all_options = dict()
+
+for c in excess_Barker:
+    l = shift_pred.loc[(shift_pred.R==c), 'cohort'].unique().tolist()
+    s = shift_pred.loc[(shift_pred.nmx.backg), 'cohort'].unique().tolist()
+    td = {c : {'year':l, 'segment':l}}
+all_options.update(td)
